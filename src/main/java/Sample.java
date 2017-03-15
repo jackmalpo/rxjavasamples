41e60404172ed0ddebd34819e772d52405d4e61b
Jack Malpasuto <jack.malpasuto@metova.com>
2017-03-15 12:48:59 -0500

    More samples

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,68 +1,80 @@
+import com.jakewharton.rxrelay.PublishRelay;
+
+import java.util.Arrays;
+
 import rx.Observable;
 import rx.functions.Action0;
 import rx.functions.Action1;
-
-import java.util.Arrays;
+import rx.subjects.BehaviorSubject;
+import rx.subjects.PublishSubject;
+import rx.subjects.ReplaySubject;
 
 public class Sample {
 
     public static void main(String[] args) {
-        just();
-
+//        just();
 //        from();
-
 //        error();
-
 //        defer();
-
 //        map();
-
+//        flatMap();
+//        publishSubject();
+//        behaviorSubject();
+//        replaySubject();
+//        errorSubject();
+        errorRelay();
     }
+
+    /** Creation **/
 
     private static void just() {
         Observable.just("Hello")
-                .subscribe(new Action1<String>() {
-                    public void call(String s) {
-                        System.out.print(s + " " + "World");
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-
-                    }
-                }, new Action0() {
-                    @Override
-                    public void call() {
-                        System.out.println("!");
-                    }
-                });
+                .subscribe(
+                        new Action1<String>() {
+                            public void call(String s) {
+                                print(s + " " + "World");
+                            }
+                        },
+                        new Action1<Throwable>() {
+                            @Override
+                            public void call(Throwable throwable) {
+                            }
+                        },
+                        new Action0() {
+                            @Override
+                            public void call() {
+                                println("!");
+                            }
+                        });
     }
 
     private static void from() {
         Observable.from(Arrays.asList("Hello", " ", "World"))
-                .subscribe(s -> System.out.print(s), throwable -> {
-                }, () -> System.out.println("!"));
+                .subscribe(s -> print(s), throwable -> {
+                }, () -> println("!"));
     }
 
     private static void error() {
         Observable.error(new RuntimeException("Uh oh"))
-                .subscribe(it -> System.out.println(it), throwable -> throwable.printStackTrace());
+                .subscribe(it -> println(it), throwable -> throwable.printStackTrace());
     }
 
     private static void defer() {
         Observable<String> just = Observable.just(printSomethingDumb());
-//        Observable<String> defer = Observable.defer(() -> Observable.just(printSomethingDumb()));
-//        Observable<String> fromCallable = Observable.fromCallable(Sample::printSomethingDumb);
+        //        Observable<String> defer = Observable.defer(() -> Observable.just(printSomethingDumb()));
+        //        Observable<String> fromCallable = Observable.fromCallable(Sample::printSomethingDumb);
 
-//        defer.subscribe();
-//        fromCallable.subscribe();
+        //        defer.subscribe();
+        //        fromCallable.subscribe();
     }
 
     private static String printSomethingDumb() {
         String x = "Something dumb";
-        System.out.println(x);
+        println(x);
         return x;
     }
+
+    /** Operators **/
 
     private static void map() {
         Observable.just("Hello, World!")
@@ -70,4 +82,56 @@ public class Sample {
                 .subscribe(System.out::println);
     }
 
+    private static void flatMap() {
+        Observable.from(Arrays.asList("A", "B", "C"))
+                .flatMap(t -> Observable.just(t + "!"))
+                .subscribe(System.out::println);
+    }
+
+    /** Subjects **/
+
+    private static void publishSubject() {
+        PublishSubject<String> publishSubject = PublishSubject.create();
+        publishSubject.subscribe(Sample::println); // Switch subscribe to after onNext
+
+//    publishSubject.onNext("Hello, world!");
+
+    }
+
+    private static void behaviorSubject() {
+        BehaviorSubject<String> behaviorSubject = BehaviorSubject.create();
+
+        behaviorSubject.onNext("Hello, World!");
+
+        behaviorSubject.subscribe(Sample::println);
+    }
+
+    private static void replaySubject() {
+        ReplaySubject<String> replaySubject = ReplaySubject.create();
+
+        replaySubject.onNext("One");
+        replaySubject.onNext("Two");
+        replaySubject.onNext("Three");
+
+        replaySubject.subscribe(Sample::println);
+    }
+
+    private static void errorSubject() {
+        PublishSubject<String> publishSubject = PublishSubject.create();
+        publishSubject.subscribe(Sample::println, Throwable::printStackTrace); // Switch subscribe to after onNext
+
+        publishSubject.onNext("Hello, world!");
+
+        publishSubject.onError(new RuntimeException("Oops"));
+
+        publishSubject.onNext("Hello again, world!");
+    }
+
+    private static void println(Object o) {
+        System.out.println(o);
+    }
+
+    private static void print(Object o) {
+        System.out.print(o);
+    }
 }
